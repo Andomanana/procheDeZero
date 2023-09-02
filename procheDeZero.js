d0a15749e57a4501a424ada1d0a5d5596113df72
Andomanana <143128080+Andomanana@users.noreply.github.com>
2023-09-02 08:16:47 +0200

    Add files via upload

---
+++ procheDeZero.js
@@ -0,0 +1,7 @@
+var a = [60,10,-0.2,-100,-4,20];
+var procheDeZero = a[0];
+for (let i=0; i<a.length; i++){
+  if (Math.abs (a[i] )< Math.abs(procheDeZero)){procheDeZero= a[i]
+ }
+}
+console.log(procheDeZero);
